cdc7bd6cb268503eab43d6ea7338233a3c3e4b11
BSH <BSH@DESKTOP-SU5F2HL>
2017-05-15 09:49:09 +0900

    실습과제 11

---
+++ JavaPractice/src/org/dimigo/inheritance/SmartPhoneTest.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *   |_ SmartPhoneTest
+ * 
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 5. 15.
+ * </pre>
+ * 
+ * @author BSH
+ * @version : 1.0
+ */
+public class SmartPhoneTest {
+
+	public static void main(String[] args) {
+		SmartPhone[] a = {
+		new IPhone("iPhone 7", "애플", 900000) , new Galaxy("갤럭시 S8", "삼성", 800000)
+		};
+		for(int i = 0; i < 2; i++){
+			func(a[i]);
+		}
+		
+	}
+	private static void func(SmartPhone a){
+		System.out.println(a);
+		a.TurnOn();
+		if(a instanceof Galaxy){
+			((Galaxy)a).pay();
+		}
+		else{
+			((IPhone)a).pay();
+		}
+		a.useSpecialFunction(a);
+		a.TurnOff();
+	}
+}	
